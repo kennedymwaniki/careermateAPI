12923d1ce3344105c6b844df0f217ad360b52706
kennedymwaniki <kennedymwaniki089@gmail.com>
2025-08-19 11:33:51 +0300

    home screen fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Public } from './auth/decorators/public.decorators';
 
+@Public()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello there, please proceed to /api to access the swagger documentation and if new go to users and create a new user and login';
   }
 }
